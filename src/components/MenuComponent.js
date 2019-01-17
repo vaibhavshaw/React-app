847525055f84a55c8d3127ada754c6a5e305eb8d
Vaibhav Shaw <vaibhavshaw028@gmail.com>
2019-01-17 23:03:03 +0530

    Components Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,50 @@
 import React, { Component } from "react";
 import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle
+} from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedDish: null
+    };
+  }
+  onSelectedDish(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div />;
   }
 
   render() {
     const menu = this.props.dishes.map(dish => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick={() => this.onSelectedDish(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+          {/* <div key={dish.id} className="col-12 mt-5">
           <Media tag="li">
             <Media left middle>
               <Media object src={dish.image} alt={dish.name} />
@@ -19,14 +54,18 @@ class Menu extends Component {
               <p>{dish.description}</p>
             </Media>
           </Media>
+      </div>  */}
         </div>
       );
     });
 
     return (
       <div className="container">
+        <div className="row">{menu}</div>
         <div className="row">
-          <Media list>{menu}</Media>
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
         </div>
       </div>
     );
